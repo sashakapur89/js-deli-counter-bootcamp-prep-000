748a4da54807e5f0d86eed40fc47e400c0efa79a
Sasha Kapur <sashakapur89@gmail.com>
2018-02-08 02:11:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine[katzDeliLine.length] = name;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  } else {
+  var firstInLine = katzDeliLine[0];
+  katzDeliLine.shift();
+  return `Currently serving ${firstInLine}.`;
+}
+}
+
+var i = 0
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  } else {
+    while(i<katzDeliLine.length){
+      return `The line is currently: ${katzDeliLine}`
+      i++
+}
+}
+}
